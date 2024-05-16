73496594f16ef286e776112b12b24fcc1c7bcd5f
chenxiaozhong <xiaozhong@mindrank.ai>
2024-05-16 18:08:37 +0800

    feat: add sql files

---
+++ mysqls/chapter_ddl.sql
@@ -0,0 +1,12 @@
+-- Active: 1715415277331@@127.0.0.1@3306@book_reader
+CREATE TABLE `chapter` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `created_at` datetime DEFAULT NULL COMMENT 'Create Time',
+  `title` varchar(255) DEFAULT NULL,
+  `content` text,
+  `book_id` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `chapter_ibfk_1` (`book_id`),
+  CONSTRAINT `chapter_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COMMENT='book chapter'
---
+++ mysqls/user_ddl.sql
@@ -0,0 +1,19 @@
+-- Active: 1715415277331@@127.0.0.1@3306@book_reader
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `password` varchar(100) NOT NULL COMMENT '登录密码',
+  `sex` varchar(1) DEFAULT NULL COMMENT '性别',
+  `tel` varchar(20) DEFAULT NULL COMMENT '手机号',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `address` varchar(200) DEFAULT NULL COMMENT '地址',
+  `status` varchar(50) DEFAULT NULL COMMENT '状态',
+  `is_super` varchar(1) DEFAULT 'N' COMMENT '是否超集管理员',
+  `created_by` varchar(100) DEFAULT NULL COMMENT '创建人',
+  `created_at` datetime DEFAULT NULL COMMENT '创建人',
+  `updated_by` varchar(100) DEFAULT NULL COMMENT '更新人',
+  `updated_at` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id_uindex` (`id`),
+  UNIQUE KEY `username_no_uindex` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='用户表'
