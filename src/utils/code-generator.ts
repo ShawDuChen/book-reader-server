1e3a457654984f0fde1ccafb53d2b7e2b97ddcaf
shawdu <528160436@qq.com>
2024-05-23 23:12:33 +0800

    feat: code generator fix

--- src/utils/code-generator.ts
+++ src/utils/code-generator.ts
@@ -8,8 +8,10 @@ export default class CodeGenerator {
   private forms_file_code: string = "";
   private api_file_code: string = "";
   table_name: string;
+  name: string;
   columns?: string | CodeColumn[];
   constructor(table_name: string, columns?: string | CodeColumn[]) {
+    this.name = table_name;
     this.table_name = `${table_name.toUpperCase().slice(0, 1)}${table_name.slice(1)}`;
     this.columns = columns;
     this.generate();
@@ -27,29 +29,32 @@ export default class CodeGenerator {
   createInterfaceCode() {
     const columns = this.columns;
     const table_name = this.table_name;
-    if (!columns || typeof columns === "string" || !columns.length) return "";
-    let interface_code: string = columns?.reduce((prev, curr) => {
-      prev += `${curr.dataIndex}: ${curr.type};\n`;
-      return prev;
-    }, `inteface ${table_name} {\n`);
-    interface_code += `}`;
-    return JSON.stringify(interface_code);
+    let interface_code: string;
+    if (!columns || typeof columns === "string" || !columns.length) {
+      interface_code = "";
+    } else {
+      interface_code = columns?.reduce((prev, curr) => {
+        prev += `${curr.dataIndex}: ${curr.type};\n`;
+        return prev;
+      }, "");
+    }
+    return `interface ${table_name} {
+${interface_code}
+}`;
   }
 
   createCrudCode() {
-    return `
-import {
+    return `import {
   create${this.table_name},
   delete${this.table_name},
   fetch${this.table_name}List,
   update${this.table_name},
-} from "@/api/business/author";
+} from "@/api/business/${this.name}";
 import { Crud } from "@/components";
 import { ${this.table_name} } from "app";
 import columns from "./modules/columns";
 import searchs from "./modules/search";
 import forms from "./modules/forms";
-
 export default function ${this.table_name}Page() {
   return (
     <Crud<${this.table_name}>
@@ -57,14 +62,13 @@ export default function ${this.table_name}Page() {
       createApi={create${this.table_name}}
       updateApi={update${this.table_name}}
       deleteApi={delete${this.table_name}}
-      queryKey="author"
+      queryKey="${this.name}"
       columns={columns}
       searchs={searchs}
       forms={forms}
     />
   );
-}    
-    `;
+}`;
   }
 
   createColumnsCode() {
@@ -76,14 +80,12 @@ export default function ${this.table_name}Page() {
             prev += `{ dataIndex: '${curr.dataIndex}', title: '${curr.title}',  },\n`;
             return prev;
           }, ``);
-    return `
-import { CrudProps } from "@/components";
+    return `import { CrudProps } from "@/components";
 import { ${this.table_name} } from "app";
 
 const columns: CrudProps<${this.table_name}>["columns"] = [
   ${insertCode}
 ];
-
 export default columns;`;
   }
 
@@ -98,8 +100,7 @@ export default columns;`;
               prev += `{ name: "${curr.dataIndex}", label: "${curr.title}", formItem: <Input placeholder="请输入" /> },\n`;
               return prev;
             }, ``);
-    return `
-import { CrudProps } from "@/components";
+    return `import { CrudProps } from "@/components";
 import { Input } from "antd";
 import { ${this.table_name} } from "app";
 
@@ -107,8 +108,7 @@ const searchs: CrudProps<${this.table_name}>["searchs"] = [
   ${insertCode}
 ];
 
-export default searchs;    
-`;
+export default searchs;`;
   }
 
   createFormsCode() {
@@ -122,8 +122,7 @@ export default searchs;
               prev += `{ name: "${curr.dataIndex}", label: "${curr.title}", formItem: <Input placeholder="请输入" /> },\n`;
               return prev;
             }, ``);
-    return `
-import { CrudProps } from "@/components";
+    return `import { CrudProps } from "@/components";
 import { Input } from "antd";
 import { ${this.table_name} } from "app";
 
@@ -131,59 +130,57 @@ const forms: CrudProps<${this.table_name}>["forms"] = [
   ${insertCode}
 ];
 
-export default forms;    
-    `;
+export default forms;
+`;
   }
 
   createApiFileCode() {
-    return `
-    import request from "@/utils/request";
-    import { ${this.table_name}, PageQuery, PageResult } from "app";
-    
-    export const fetch${this.table_name}List = (params: PageQuery<Partial<${this.table_name}>>) => {
-      return request<PageResult<${this.table_name}>>({
-        url: "/author",
-        method: "get",
-        params,
-      });
-    };
-    
-    export const get${this.table_name} = (id: number) => {
-      return request<${this.table_name}>({
-        url: \`/author/\${id}\`,
-        method: "get",
-      });
-    };
-    
-    export const create${this.table_name} = (data: ${this.table_name}) => {
-      return request<${this.table_name}>({
-        url: "/author",
-        method: "post",
-        data,
-      });
-    };
-    
-    export const update${this.table_name} = (data: ${this.table_name}) => {
-      return request<${this.table_name}>({
-        url: \`/author/\${data.id}\`,
-        method: "put",
-        data,
-      });
-    };
-    
-    export const delete${this.table_name} = (id: number) => {
-      return request<${this.table_name}>({
-        url: \`/author/\${id}\`,
-        method: "delete",
-      });
-    };    
-    `;
+    return `import request from "@/utils/request";
+import { ${this.table_name}, PageQuery, PageResult } from "app";
+
+export const fetch${this.table_name}List = (params: PageQuery<Partial<${this.table_name}>>) => {
+  return request<PageResult<${this.table_name}>>({
+    url: "/${this.name}",
+    method: "get",
+    params,
+  });
+};
+
+export const get${this.table_name} = (id: number) => {
+  return request<${this.table_name}>({
+    url: \`/${this.name}/\${id}\`,
+    method: "get",
+  });
+};
+
+export const create${this.table_name} = (data: ${this.table_name}) => {
+  return request<${this.table_name}>({
+    url: "/${this.name}",
+    method: "post",
+    data,
+  });
+};
+
+export const update${this.table_name} = (data: ${this.table_name}) => {
+  return request<${this.table_name}>({
+    url: \`/${this.name}/\${data.id}\`,
+    method: "put",
+    data,
+  });
+};
+
+export const delete${this.table_name} = (id: number) => {
+  return request<${this.table_name}>({
+    url: \`/${this.name}/\${id}\`,
+    method: "delete",
+  });
+};`;
   }
 
   getCode() {
     return {
       table_name: this.table_name,
-      columns: this.columns,
+      columns: this.columns || [],
       interface_code: this.interface_code,
       crud_file_code: this.crud_file_code,
       columns_file_code: this.columns_file_code,
